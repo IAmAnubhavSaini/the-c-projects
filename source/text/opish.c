323a5e20f7d8c0bb2f19645150968b74c62179c8
IAmAnubhavSaini <iamanubhavsaini@gmail.com>
2013-07-22 14:15:26 +0530

    opish working. 1 down. v1

---
+++ source/text/opish.c
@@ -0,0 +1,37 @@
+/* Anubhav Saini
+* read: wiki/pig_latin
+* opish becomes opopisophop : add "op" after every consonant.
+*/
+
+#include<stdio.h>
+
+#define LOWER(c)   (c | 0x20)
+#define ISALPHA(c) (LOWER(c)>='a'&&LOWER(c)<='z')
+char vowels[5] = { 'a', 'e', 'i', 'o', 'u' };
+
+void main(){
+  char suffix1 = 'o', suffix2='p';
+  char input;
+  int i = 0;
+  int flagIsConsonant = 1;
+  printf("\nKeep entering letters to convert to pig latin: ");
+  while(scanf("%c",&input) && input != EOF){
+    if(ISALPHA(input)){
+      flagIsConsonant = 1;
+      for(i = 0; i < 5; i++){
+        if(input == vowels[i]){
+          flagIsConsonant = 0;
+          break;
+        }
+      }
+      printf("%c",input);
+      if(flagIsConsonant){
+        putchar(suffix1);
+        putchar(suffix2);
+      }
+    }
+    else{
+      puts(" ");
+    }
+  }
+}
